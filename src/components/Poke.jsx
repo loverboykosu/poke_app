d931c2b500d8a8479b138cd4faee333f5d723037
loverboy <loverboykosu@gmail.com>
2025-03-09 21:07:59 +0900

    feat: add images

--- src/components/Poke.jsx
+++ src/components/Poke.jsx
@@ -5,19 +5,32 @@ import Page from "./Page.jsx";
 const Poke = () => {
   const [page, setPage] = useState(0);
   const [pokeList, setPokeList] = useState([]);
-  const [pokeImage, setPokeImage] = useState();
+  const [pokeImage, setPokeImage] = useState([]);
   const [isLoad, setIsLoad] = useState(false);
   useEffect(() => {
     fetchPokeList();
   }, [page]);
+
+  useEffect(() => {
+    fetchPokeImage();
+  }, [pokeList]);
   const fetchPokeList = async () => {
     const offset = page * 20;
     const apiUrl = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offset}`;
     const result = await axios.get(apiUrl);
     setPokeList(result.data.results);
-    setIsLoad((prev) => {
-      return !prev;
-    });
+  };
+  const fetchPokeImage = async () => {
+    if (pokeList.length) {
+      const pokeImages = await Promise.all(
+        pokeList.map(async (item) => {
+          const response = await axios.get(item.url);
+          return response.data.sprites.front_default; // 画像URLを取得
+        })
+      );
+      setPokeImage(pokeImages); // 画像URLの配列をセット
+    }
+    setIsLoad(true);
   };
   const nextPage = () => {
     setPage(page + 1);
@@ -32,11 +45,15 @@ const Poke = () => {
         <h1 className="text-white text-center p-5 text-2xl">Poke app</h1>
       </div>
       <div className="grid grid-cols-4 gap-10">
-        {/* <img src={isLoad ? pokeImage : null} alt="" /> */}
-        {pokeList.map((item) => {
+        {pokeList.map((item, index) => {
           return (
             <div key={item.name} className="text-center">
               <p>{item.name}</p>
+              <img
+                src={isLoad ? pokeImage[index] : null}
+                alt=""
+                className="mx-auto"
+              />
               <div className="text-sky-400">
                 <a href={item.url}>{item.url}</a>
               </div>
