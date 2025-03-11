b4a476aeaad7a17b30ace8a0d36c34018e8dcac2
loverboy <loverboykosu@gmail.com>
2025-03-11 23:39:05 +0900

    feat: add search button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
 const Search = () => {
+  const [searchWord, setSearchWord] = useState("");
+  const getInput = (e) => {
+    setSearchWord(e.target.value);
+  };
   return (
     <>
       <section className="flex justify-end">
@@ -10,8 +15,12 @@ const Search = () => {
             type="text"
             placeholder="検索ワード"
             className="bg-gray-200 focus:outline-none w-64"
+            onChange={getInput}
           />
         </div>
+        <button className="bg-gray-800 hover:bg-gray-200 text-white font-bold py-2 px-4 rounded">
+          Search
+        </button>
       </section>
     </>
   );
